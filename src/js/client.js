26b78c1304838dd1b52798127b8a467403ffbd66
danb-cws <sugarlumpltd@gmail.com>
2016-03-21 23:13:39 +0000

    touch

--- src/js/client.js
+++ src/js/client.js
@@ -2,7 +2,7 @@
 
 import * as greet from './testvar';
 
-// require('../sass/alt.scss');
+//  require('../sass/alt.scss');
 import '../sass/alt.scss';
 
 console.log(`hello, greet name: ${greet.name}`);
