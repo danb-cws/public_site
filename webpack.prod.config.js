e2e2973071dbeda762c49d2d9b6e2a45954638af
danb-cws <sugarlumpltd@gmail.com>
2016-03-22 01:18:04 +0000

    test remove occurrence order plugin

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -8,8 +8,7 @@ var webpack = require('webpack'),
 module.exports = {
     context: __dirname,
     entry:[
-        path.join(__dirname, '/src/js/client.js')
-       // './src/js/client.js'
+        './src/js/client.js'
     ],
     output: {
         path: path.join(__dirname, '/dist/js'),
@@ -39,7 +38,7 @@ module.exports = {
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new ExtractTextPlugin('../css/[name].[hash].css'),
-        new webpack.optimize.OccurenceOrderPlugin(),
+        //new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({minimize: true})
     ],
     resolve: {
