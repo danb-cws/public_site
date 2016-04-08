b44061f47b7d064aa35fa0f92f7fa8e31dbe5e27
danb-cws <sugarlumpltd@gmail.com>
2016-04-08 17:15:00 +0100

    testing some css4 via cssnext

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,7 @@
 // var greet = require('./testvar');
+import '../sass/base.scss';
 import * as greet from './testvar';
 
-import '../sass/base.scss';
 // console.log(`client.js: hello, greet name: ${greet.name}`);
 greet.sayName();
 greet.sayName2();
